53a2a6fd8bce242f144330f6d53bdacc7a95e514
Nick Senger <dev@nsenger.com>
2022-05-04 11:46:15 -0700

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Iced Workshop
+
+Welcome to the Iced GUI workshop!
+
+Over the course of the hour we will be building a lightweight chat client (a trollbox) in Rust.
+
+Before getting started, here are a few steps to complete on your own:
+
+- Install rustup: https://rustup.rs/
+- Clone this repository, navigate to it, and run `cargo check`. This will retrieve the dependencies and ensure that your Rust installation is good to go.
+
+### Part 1 - Familiarization
+
+- Lifecycle of an Iced application
+- Review of the Application trait
+- view -> update -> input (explore using print)
---
+++ client/src/main.rs
@@ -0,0 +1,53 @@
+use iced::executor;
+use iced::pure::{text, Application, Element};
+use iced::{Command, Settings, Subscription};
+
+mod client;
+
+use client::Client;
+
+pub fn main() -> iced::Result {
+    State::run(Settings::default())
+}
+
+enum State {
+    Disconnected,
+    Connected,
+}
+
+#[derive(Clone, Debug)]
+pub enum Message {
+    Connected(Client),
+    Disconnected,
+    TrollBoxMessageReceived(schema::Message),
+}
+
+impl Application for State {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (Self::Disconnected, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Trollbox - Iced Workshop")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        println!("message received: {:?}", message);
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        println!("produced a view");
+
+        text("Soon™").into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Client::connect()
+    }
+}
