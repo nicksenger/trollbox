f2cc97acdbda66e22ceb5a016e5ac4ee569b403c
Nick Senger <dev@nsenger.com>
2022-05-04 11:46:15 -0700

    initial

---
+++ client/src/main.rs
@@ -0,0 +1,67 @@
+use iced::executor;
+use iced::pure::{column, scrollable, text, Application, Element};
+use iced::{Command, Settings, Subscription};
+
+mod client;
+
+use client::Client;
+
+pub fn main() -> iced::Result {
+    State::run(Settings::default())
+}
+
+enum State {
+    Disconnected,
+    Connected { messages: Vec<schema::Message> },
+}
+
+#[derive(Clone, Debug)]
+pub enum Message {
+    Connected(Client),
+    Disconnected,
+    TrollBoxMessageReceived(schema::Message),
+}
+
+impl Application for State {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (Self::Disconnected, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Trollbox - Iced Workshop")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Connected(_) => *self = State::Connected { messages: vec![] },
+            Message::Disconnected => *self = State::Disconnected,
+            Message::TrollBoxMessageReceived(message) => {
+                if let State::Connected { messages } = self {
+                    messages.push(message);
+                }
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        match self {
+            Self::Connected { messages } => {
+                scrollable(messages.iter().fold(column(), |column, message| {
+                    column.push(text(format!("{}: {}", message.alias, message.text)))
+                }))
+                .into()
+            }
+            Self::Disconnected => text("Connecting to the trollbox...").into(),
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Client::connect()
+    }
+}
